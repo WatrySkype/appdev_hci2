36dfcced84d58fe1ca7d12ec7004547bfa148876
IcySkype <skytrizero@gmail.com>
2023-05-14 17:00:45 +0800

    updated screen to have sidebar menu

--- data/classes/Board.py
+++ data/classes/Board.py
@@ -5,12 +5,13 @@ from data.classes.pieces.ranked import *
 from data.classes.pieces.flag import Flag
 from data.classes.pieces.spy import Spy
 from data.classes.pieces.private import Private
+from data.classes.Logic import get_rank
 
 class Board:
     def __init__(self, width, height):
-        self.width = width
+        self.width = (width*2/3)
         self.height = height
-        self.tile_width = width // 9
+        self.tile_width = (width*2/3) // 9
         self.tile_height = height // 8
         self.selected_piece = None
         self.turn = 'white'
@@ -74,6 +75,11 @@ class Board:
             if clicked_square.occupying_piece is not None:
                 if clicked_square.occupying_piece.color == self.turn:
                     self.selected_piece = clicked_square.occupying_piece
+                else:
+                    #user clicks opponent piece to see possible ranks of that piece
+                    for x in clicked_square.occupying_piece.opponent_guess:
+                        print(get_rank(x))
+                    print('actual: '+clicked_square.occupying_piece.notation)
         elif self.selected_piece.move(self, clicked_square):
             self.turn = 'white' if self.turn == 'black' else 'black'
         elif clicked_square.occupying_piece is not None:
